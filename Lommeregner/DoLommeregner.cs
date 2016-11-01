ac46c1bbefb71bfe73f804418c8d3acd74f80137
Anders Thomsen <ande605x@edu.easj.dk>
2016-11-01 10:49:44 +0100

    Tilføjet while loop

--- Lommeregner/DoLommeregner.cs
+++ Lommeregner/DoLommeregner.cs
@@ -12,21 +12,37 @@ namespace Lommeregner
         public void MyCode()
         {
 
+            Calculate calcu = new Calculate();
+
             Console.WriteLine("Velkommen til Lommeregner");
             Console.WriteLine("=========================");
             Console.WriteLine();
 
 
-            Console.WriteLine("Indtast tal 1: ");
-            string input1 = Console.ReadLine();
-            int tal1 = Int32.Parse(input1);
+            string sentinel = "y";
 
-            Console.WriteLine("Indtast en matematisk oprator: ");
-            string input2 = Console.ReadLine();
+            while (sentinel != "n")
+            {
+                Console.WriteLine("Indtast tal 1: ");
+                string input1 = Console.ReadLine();
+                int tal1 = Int32.Parse(input1);
 
-            Console.WriteLine("Indtast tal 2: ");
-            string input3 = Console.ReadLine();
-            int tal2 = Int32.Parse(input2);
+                Console.WriteLine("Indtast en matematisk oprator: ");
+                string input2 = Console.ReadLine();
+
+                Console.WriteLine("Indtast tal 2: ");
+                string input3 = Console.ReadLine();
+                int tal2 = Int32.Parse(input3);
+
+                Console.WriteLine($"Resultat: " + calcu.mathcalc(tal1, tal2, input2));
+
+                Console.WriteLine("Forsæt [y/n]: ");
+                sentinel = Console.ReadLine();
+                Console.WriteLine();
+
+            }
+
+          
 
 
             //int tal2 = Int32.Parse(input2);
